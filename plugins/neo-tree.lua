d9827031a262c95065a125725e9a5eb38dea8d4b
Kyle Benne <Kyle.Benne@nrel.gov>
2023-05-11 11:05:36 -0500

    neo-tree winbar = false

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -68,7 +68,7 @@ return {
       auto_clean_after_session_restore = true,
       close_if_last_window = true,
       source_selector = {
-        winbar = true,
+        winbar = false,
         content_layout = "center",
         tab_labels = {
           filesystem = get_icon "FolderClosed" .. " File",
